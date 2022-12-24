5572765ea8991d280a8a2af69c0e1198aee53bf4
Armin Eskandarinasab <armin3731@live.com>
2022-12-24 15:22:10 +0330

    initial commit of the project

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+import { MathService } from './math.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService, private mathService: MathService) {}
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('add')
+  async accumulate(@Body('data') data: number[]){
+    //Define the logic to be executed
+    console.log('Adding ' + data.toString());
+    return this.mathService.accumulate(data);
+
+  }
+}
