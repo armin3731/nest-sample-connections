9814e29b044949407f73ae0528f2fc73dc639dcb
Armin Eskandarinasab <armin3731@live.com>
2022-12-25 11:23:23 +0330

    edit: define Microservice1 as a microservice

--- Microservice1/src/main.ts
+++ Microservice1/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+
+// create a logger
+const logger = new Logger('Main');
+
+
+// Create the Microservice options object
+const microserviceOptions = {
+  transport: Transport.TCP,
+  options: {
+    host: '127.0.0.1',
+    port: 8877
+  }
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule,microserviceOptions)
+  // app.listen(() => {
+  //   logger.log('Microservice1 is listening...');
+  // });
+  app.listen();
 }
 bootstrap();
