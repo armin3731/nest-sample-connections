71a9ad1a58ec85347e3f0fc8378f9c07f4e95a7f
Armin Eskandarinasab <armin3731@live.com>
2022-12-25 09:02:16 +0330

    bugfix: fixing MathService to app.module and controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,8 @@ import { MathService } from './math.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService, private mathService: MathService) {}
+
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
@@ -13,7 +15,7 @@ export class AppController {
   @Post('add')
   async accumulate(@Body('data') data: number[]){
     //Define the logic to be executed
-    console.log('Adding ' + data.toString());
+    console.log('Adding ' + data);
     return this.mathService.accumulate(data);
 
   }
